aa058e6ffa681264a9efc80a6d17b9962f595554
胡汉权 <huhanquan@hisec.com.cn>
2017-08-27 22:00:38 +0800

    no message

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 'use strict'
+const koa = require('koa.io');
+const app = koa();
+const PORT = 8088;
+const room = "test";
+app.io.use(function *(next){
+    this.join(room, () => {
+        console.log(this.rooms);
+    });
+    yield next;
+});
+app.io.route('message', function *(){
+    console.log('receive:', this.data);
+    app.io.to(room).emit('message', this.data);
+});
+app.listen(PORT, () => {
+    console.log('Server listening at port %d', PORT);
+})
